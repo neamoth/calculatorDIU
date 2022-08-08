dc2d5e0022bcecc7b4eaba0a2261abff46bd1159
Neamoth <neamothku@gmail.com>
2022-08-08 17:05:31 +0600

    Fix: Crush error for empty field

--- app/src/main/java/com/example/calculatordiu/MainActivity.java
+++ app/src/main/java/com/example/calculatordiu/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.calculatordiu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText numoneid, numtwoid;
@@ -25,30 +27,66 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sumFunction(View view) {
-        num1 = Double.parseDouble(numoneid.getText().toString());
-        num2 = Double.parseDouble(numtwoid.getText().toString());
-        result = num1+num2;
-        resultID.setText("Result: "+result);
+
+        if(TextUtils.isEmpty(numoneid.getText().toString())){
+            Toast.makeText(MainActivity.this, "Enter first value and try again..!", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(numtwoid.getText().toString())){
+            Toast.makeText(MainActivity.this, "Enter second value and try again..!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            num1 = Double.parseDouble(numoneid.getText().toString());
+            num2 = Double.parseDouble(numtwoid.getText().toString());
+            result = num1+num2;
+            resultID.setText("Result: "+result);
+        }
     }
 
     public void subFunction(View view) {
-        num1 = Double.parseDouble(numoneid.getText().toString());
-        num2 = Double.parseDouble(numtwoid.getText().toString());
-        result = num1-num2;
-        resultID.setText("Result: "+result);
+        if(TextUtils.isEmpty(numoneid.getText().toString())){
+            Toast.makeText(MainActivity.this, "Enter first value and try again..!", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(numtwoid.getText().toString())){
+            Toast.makeText(MainActivity.this, "Enter second value and try again..!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            num1 = Double.parseDouble(numoneid.getText().toString());
+            num2 = Double.parseDouble(numtwoid.getText().toString());
+            result = num1-num2;
+            resultID.setText("Result: "+result);
+        }
+
     }
 
     public void multFunction(View view) {
-        num1 = Double.parseDouble(numoneid.getText().toString());
-        num2 = Double.parseDouble(numtwoid.getText().toString());
-        result = num1*num2;
-        resultID.setText("Result: "+result);
+
+        if(TextUtils.isEmpty(numoneid.getText().toString())){
+            Toast.makeText(MainActivity.this, "Enter first value and try again..!", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(numtwoid.getText().toString())){
+            Toast.makeText(MainActivity.this, "Enter second value and try again..!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            num1 = Double.parseDouble(numoneid.getText().toString());
+            num2 = Double.parseDouble(numtwoid.getText().toString());
+            result = num1*num2;
+            resultID.setText("Result: "+result);
+        }
     }
 
     public void divFunction(View view) {
-        num1 = Double.parseDouble(numoneid.getText().toString());
-        num2 = Double.parseDouble(numtwoid.getText().toString());
-        result = num1/num2;
+
         resultID.setText("Result: "+result);
+        if(TextUtils.isEmpty(numoneid.getText().toString())){
+            Toast.makeText(MainActivity.this, "Enter first value and try again..!", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(numtwoid.getText().toString())){
+            Toast.makeText(MainActivity.this, "Enter second value and try again..!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            num1 = Double.parseDouble(numoneid.getText().toString());
+            num2 = Double.parseDouble(numtwoid.getText().toString());
+            result = num1/num2;
+        }
     }
 }
